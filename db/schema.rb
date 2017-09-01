e4b2d9ddaf87d418cde6fc3eda225d7ca047fe67
L. Soai-Van <leo.soaivan@gmail.com>
2017-09-01 13:15:45 -0400

    Add :accepted to Friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827203128) do
+ActiveRecord::Schema.define(version: 20170901013912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170827203128) do
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted", default: false
   end
 
   create_table "users", force: :cascade do |t|
