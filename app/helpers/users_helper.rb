509f4bacf8359295089adc5d1712d732c7013333
L. Soai-Van <leo.soaivan@gmail.com>
2017-09-21 23:14:04 -0400

    Add user gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
   def find_user(id)
     User.find(id)
   end
+
+  def gravatar_for(user, size: 50)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
 end
