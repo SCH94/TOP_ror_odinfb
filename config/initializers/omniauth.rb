24267f591391c234e6124cdc36b11e8478a072a3
L. Soai-Van <leo.soaivan@gmail.com>
2017-09-25 21:26:21 -0400

    Fix OAuth issue

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+# OmniAuth.config.logger = Rails.logger
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FB_APP_ID'], 
+#                       ENV['FB_APP_SECRET']
+#   provider :google_oauth2, ENV['GOOGLE_APP_ID'],
+#                            ENV['GOOGLE_APP_SECRET']
+# end
