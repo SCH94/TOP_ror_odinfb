ad435c0b646ff4175dbccd3bf70ba5351466c895
L. Soai-Van <leo.soaivan@gmail.com>
2017-11-26 00:41:11 -0500

    Fix users/show and tests

--- spec/features/posting_spec.rb
+++ spec/features/posting_spec.rb
@@ -9,7 +9,7 @@ describe 'Post management', type: :feature do
   context 'from the feed' do
     scenario 'creating a post' do
       visit authenticated_root_path
-      click_link 'Create post'
+      click_link 'Create a post'
       expect{
         fill_in 'Title', with: 'A new post'
         fill_in 'Post', with: 'This is a brief, test post.'
@@ -21,7 +21,7 @@ describe 'Post management', type: :feature do
 
     scenario 'cancelling a post' do
       visit authenticated_root_path
-      click_link 'Create post'
+      click_link 'Create a post'
       click_link 'Cancel'
       expect(current_path).to eq authenticated_root_path
     end
@@ -30,7 +30,7 @@ describe 'Post management', type: :feature do
   context 'from a profile' do
     scenario 'creating a post' do
       visit user_path(@user)
-      click_link 'Create post'
+      click_link 'Create a post'
       expect{
         fill_in 'Title', with: 'A new post'
         fill_in 'Post', with: 'This is a brief, test post.'
@@ -42,7 +42,7 @@ describe 'Post management', type: :feature do
 
     scenario 'cancelling a post' do
       visit user_path(@user)
-      click_link 'Create post'
+      click_link 'Create a post'
       click_link 'Cancel'
       expect(current_path).to eq user_path(@user)
     end
