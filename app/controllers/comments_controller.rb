e8492efa210866691f52f65ce47da331cd47b506
L. Soai-Van <leo.soaivan@gmail.com>
2017-11-27 19:15:46 -0500

    Add :set_post callback to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: :create
+
   def new
     @comment = Comment.new
   end
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @poster = User.find(@post.user_id)
-
     if @comment.save
       redirect_to session[:return_to]
     else
@@ -17,6 +16,9 @@ class CommentsController < ApplicationController
   end
 
   private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
 
     def comment_params
       params.require(:comment).permit(:body).merge(user_id: current_user.id)
